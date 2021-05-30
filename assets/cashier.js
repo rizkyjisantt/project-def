57214092142089671ef726e0cafe9836c546b06c
Bachtiar Dwi Yusril R <84233153+bchtryusril17@users.noreply.github.com>
2021-05-30 09:28:11 +0700

    menambahkan function untuk harga barang

--- assets/cashier.js
+++ assets/cashier.js
@@ -1,1 +1,11 @@
-
+function munculhargabarang(){
+  var tujuan = (document.form.input_barang.value);
+  if (tujuan == "baju"){hargabarang = 75000;}
+  else if(tujuan == "jacket"){hargabarang = 350000;}
+  else if(tujuan == "kemeja"){hargabarang = 150000;}
+  else if(tujuan == "sweater"){hargabarang = 120000;}
+  else if(tujuan == "celana"){hargabarang = 100000;}
+  else if(tujuan == "sepatu"){hargabarang = 250000;}
+  else if(tujuan == "sandal"){hargabarang = 15000;}
+  document.form.output_hargabarang.value=eval(hargabarang);
+}
